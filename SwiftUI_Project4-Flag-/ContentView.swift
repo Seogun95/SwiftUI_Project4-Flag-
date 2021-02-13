1cc3bb4feeb23cc561c77da28d9d95c1c16b4e5d
SeonJungKim <ksj083895@gmail.com>
2021-02-13 17:38:10 +0500

    Flag Project

--- SwiftUI_Project4-Flag-/ContentView.swift
+++ SwiftUI_Project4-Flag-/ContentView.swift
@@ -1,16 +1,71 @@
-//
-//  ContentView.swift
-//  SwiftUI_Project4-Flag-
-//
-//  Created by 김선중 on 2021/02/13.
-//
+
 
 import SwiftUI
 
 struct ContentView: View {
+    /*국기를 나열하고, shuffled로 배열을 랜덤하게설정
+     Assets에 있는 사진 이름을 가져옴 */
+    @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
+    
+    //국기의 이름을 랜덤하게 3개까지 보여줌
+    @State private var correctAnswer = Int.random(in: 0...2)
+
+    @State private var showingScore = false
+    @State private var scoreTitle = ""
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            LinearGradient(gradient: Gradient(colors: [.red, .blue])
+                     , startPoint: .leading, endPoint: .trailing)
+                .edgesIgnoringSafeArea(.all)
+        
+
+            VStack(spacing: 30) {
+                VStack {
+                    Text("알맞는 국기를 선택하세요")
+                        .foregroundColor(.white)
+
+                    Text(countries[correctAnswer])
+                        .foregroundColor(.white)
+                        .font(.largeTitle)
+                        .fontWeight(.black)
+                }
+
+                ForEach(0 ..< 3) { number in
+                    Button(action: {
+                        self.flagTapped(number)
+                    }) {
+                        Image(self.countries[number])
+                            .renderingMode(.original)
+                            .clipShape(Capsule())
+                            .overlay(Capsule().stroke(Color.black, lineWidth: 1))
+                            .shadow(color: .black, radius: 2)
+                    }
+                }
+
+                Spacer()
+            }
+        }
+        .alert(isPresented: $showingScore) {
+            Alert(title: Text(scoreTitle), message: Text("Your score is ???"), dismissButton: .default(Text("Continue")) {
+                self.askQuestion()
+            })
+        }
+    }
+
+    func flagTapped(_ number: Int) {
+        if number == correctAnswer {
+            scoreTitle = "Correct"
+        } else {
+            scoreTitle = "Wrong"
+        }
+
+        showingScore = true
+    }
+
+    func askQuestion() {
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
     }
 }
 
